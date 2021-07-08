440e11352c9716b86a4f6bfc55f117974e4618c3
Nikolay <k0jl9ih@yandex.ru>
2021-07-08 20:04:38 +0300

    week2 ready

--- tours/views.py
+++ tours/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
+import random
 from data import tours, departures
 # Create your views here.
 # Для рендеринга меню departures передается каждому view, честно говоря, не понял как сделать элегантнее.
 
+
 def main_view(request):
-    return render(request, 'tours/index.html', {"departures": departures})
+    # создаем случайный список из 6 туров
+    random_6_tours = dict(random.sample(tours.items(), 6))
+    return render(request, 'tours/index.html', {"random_6_tours": random_6_tours,  # 6 случайных туров
+                                                "departures": departures  # места отправления для формирования меню
+                                                })
 
 
 def departure_view(request, departure: str):
@@ -27,11 +33,12 @@ def departure_view(request, departure: str):
         "minimum": min([tour.get("nights") for tour in departure_tours.values()])
     }
 
-    return render(request, 'tours/departure.html', {"dep_tours": departure_tours,
-                                                    "departure": departures.get(departure),
-                                                    "costs": costs,
-                                                    "nights": nights,
-                                                    "departures": departures})
+    return render(request, 'tours/departure.html', {"dep_tours": departure_tours,  # туры из места отправления
+                                                    "departure": departures.get(departure),  # место вылета
+                                                    "costs": costs,  # словарь с минимальной и максимальной стоимостями
+                                                    "nights": nights,  # словарь с мин и макс количеством ночей
+                                                    "departures": departures  # места отправления для формирования меню
+                                                    })
 
 
 def tour_view(request, tour_id: int):
@@ -42,10 +49,11 @@ def tour_view(request, tour_id: int):
 
     star_range = range(int(tours.get(tour_id).get("stars")))
     departure = departures.get(tours.get(tour_id).get("departure"))
-    return render(request, 'tours/tour.html', {"tour": tours.get(tour_id),
-                                               "star_range": star_range,
-                                               "departure": departure,
-                                               "departures": departures})
+    return render(request, 'tours/tour.html', {"tour": tours.get(tour_id),  # тур по tour_id
+                                               "star_range": star_range,  # range для количества звезд
+                                               "departure": departure,  # место вылета
+                                               "departures": departures  # места отправления для формирования меню
+                                               })
 
 
 def custom_handler404(request, exception):
