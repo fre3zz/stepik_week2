789f0743d65a10c0f8f880cf74c8c5006ca628b7
Nikolay <k0jl9ih@yandex.ru>
2021-07-08 20:43:14 +0300

    allready working

--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.http import HttpResponseNotFound, HttpResponseServerError, Http404
 from django.shortcuts import render
 import random
 from data import tours, departures
@@ -22,7 +22,7 @@ def departure_view(request, departure: str):
 
     # передаем departures для рендера меню
 
-    departure_tours = {tour_id: tour for (tour_id, tour) in tours.items() if tour.get("departure") == departure}
+    departure_tours = {tour_id: tour for (tour_id, tour) in tours.items() if tour["departure"] == departure}
 
     costs = {
         "maximum": max([tour.get("price") for tour in departure_tours.values()]),
@@ -32,13 +32,16 @@ def departure_view(request, departure: str):
         "maximum": max([tour.get("nights") for tour in departure_tours.values()]),
         "minimum": min([tour.get("nights") for tour in departure_tours.values()])
     }
-
-    return render(request, 'tours/departure.html', {"dep_tours": departure_tours,  # туры из места отправления
-                                                    "departure": departures.get(departure),  # место вылета
-                                                    "costs": costs,  # словарь с минимальной и максимальной стоимостями
-                                                    "nights": nights,  # словарь с мин и макс количеством ночей
-                                                    "departures": departures  # места отправления для формирования меню
-                                                    })
+    if departure not in departures.keys():
+        print("123123")
+        raise Http404
+    else:
+        return render(request, 'tours/departure.html', {"dep_tours": departure_tours,  # туры из места отправления
+                                                        "departure": departures.get(departure),  # место вылета
+                                                        "costs": costs,  # словарь с мин и макс стоимостями
+                                                        "nights": nights,  # словарь с мин и макс количеством ночей
+                                                        "departures": departures  # места отправления для меню
+                                                        })
 
 
 def tour_view(request, tour_id: int):
@@ -47,9 +50,14 @@ def tour_view(request, tour_id: int):
     # Также передается город из которого вылет
     # Departures передается для рендера меню
 
-    star_range = range(int(tours.get(tour_id).get("stars")))
-    departure = departures.get(tours.get(tour_id).get("departure"))
-    return render(request, 'tours/tour.html', {"tour": tours.get(tour_id),  # тур по tour_id
+    try:
+        tour = tours[tour_id]
+        star_range = range(int(tour["stars"]))
+        departure = departures.get(tour["departure"])
+    except KeyError:
+        raise Http404
+
+    return render(request, 'tours/tour.html', {"tour": tour,  # тур по tour_id
                                                "star_range": star_range,  # range для количества звезд
                                                "departure": departure,  # место вылета
                                                "departures": departures  # места отправления для формирования меню
